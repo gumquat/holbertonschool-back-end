9ab1619e28cbf4f734c284437e26c90400cc8bb3
gumquat <gumquat@gmail.com>
2023-07-20 12:34:57 -0500

    revert

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,42 +10,32 @@ import requests
 import sys
 
 
-def get_employee_tasks(employeeId):
-    """Get the tasks of an employee"""
-    url = "https://jsonplaceholder.typicode.com/"
-    url += "users/{}/todos".format(employeeId)
+def get_employee_todo_progress(employee_id):
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+
     response = requests.get(url)
-    return response.json()
+    todo_list = response.json()
 
+    employee_name = ""
+    total_tasks = len(todo_list)
+    done_tasks = 0
+    completed_task_titles = []
 
-def get_employee_name(employeeId):
-    """Get the name of an employee"""
-    url = "https://jsonplaceholder.typicode.com/"
-    url += "users/{}".format(employeeId)
-    response = requests.get(url)
-    return response.json().get("name")
+    for task in todo_list:
+        if task['completed']:
+            done_tasks += 1
+            completed_task_titles.append(task['title'])
+        if 'username' in task:
+            employee_name = task['username']
 
+    print(f"Employee\
+           {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
 
-def get_completed_tasks(tasks):
-    """Get the completed tasks of an employee"""
-    completed_tasks = []
-    for task in tasks:
-        if task.get("completed"):
-            completed_tasks.append(task)
-    return completed_tasks
-
-
-def print_employee_tasks(employeeName, completedTasks, totalTasks):
-    """Print the tasks of an employee"""
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, len(completedTasks), totalTasks))
-    for task in completedTasks:
-        print("\t {}".format(task.get("title")))
-
+    if done_tasks > 0:
+        print("Completed tasks:")
+        for title in completed_task_titles:
+            print(f"\t{title}")
 
 if __name__ == "__main__":
-    employeeId = sys.argv[1]
-    tasks = get_employee_tasks(employeeId)
-    employeeName = get_employee_name(employeeId)
-    completedTasks = get_completed_tasks(tasks)
-    print_employee_tasks(employeeName, completedTasks, len(tasks))
+    if len(sys.argv) != 2:
+        employee_id = int(sys.argv[1])
